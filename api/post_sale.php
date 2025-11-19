3c03b52649c89fdde363bb4a355b93c569ee3745
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-11-19 18:56:19 -0300

    nothing fix

--- api/post_sale.php
+++ api/post_sale.php
@@ -3,7 +3,7 @@ require_once 'conn.php';
 
 header("Access-Control-Allow-Methods: POST,OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Headers:*");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);

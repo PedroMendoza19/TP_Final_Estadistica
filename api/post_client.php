f5388d5619f03ce11cfea305eb70402e309898ee
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-11-19 19:33:45 -0300

    Remove duplicate Content-Type header in post_client.php
    Removed duplicate Content-Type header.

--- api/post_client.php
+++ api/post_client.php
@@ -4,7 +4,6 @@ require_once 'conn.php';
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers:*");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
